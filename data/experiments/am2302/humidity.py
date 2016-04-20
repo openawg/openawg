cf4c6138c2340c652f210212700fc508a0c7075a
clay richardson <clayrichardson@users.noreply.github.com>
2016-04-19 22:33:01 -0700

    modified for 3 sensors

--- data/experiments/am2302/humidity.py
+++ data/experiments/am2302/humidity.py
@@ -19,7 +19,9 @@ DHT_TYPE = Adafruit_DHT.AM2302
 # Example of sensor connected to Raspberry Pi pin 23
 # DHT_PIN = 4
 # Example of sensor connected to Beaglebone Black pin P8_11
-DHT_PIN = 'P8_11'
+DHT_PIN1 = 'P8_11'  # exit sensor
+DHT_PIN2 = 'P8_12'  # front sensor
+DHT_PIN3 = 'P8_14'  # ambient sensor
 
 # How long to wait (in seconds) between measurements.
 FREQUENCY_SECONDS = 2
@@ -29,30 +31,65 @@ DATAFILE = sys.argv[1]
 print "appending to file: %s" % (DATAFILE)
 
 while True:
+    humidity1 = None
+    temp1 = None
+    humidity2 = None
+    temp2 = None
+    humidity3 = None
+    temp3 = None
     # Attempt to get sensor reading.
-    humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+    while humidity1 is None or temp1 is None:
+        print "trying sensor1..."
+        humidity1, temp1 = Adafruit_DHT.read(DHT_TYPE, DHT_PIN1)
+        time1 = time.time()
+    print "got sensor1 data"
+    while humidity2 is None or temp2 is None:
+        print "trying sensor2..."
+        humidity2, temp2 = Adafruit_DHT.read(DHT_TYPE, DHT_PIN2)
+        time2 = time.time()
+    print "got sensor2 data"
+    while humidity3 is None or temp3 is None:
+        print "trying sensor3..."
+        humidity3, temp3 = Adafruit_DHT.read(DHT_TYPE, DHT_PIN3)
+        time3 = time.time()
+    print "got sensor3 data"
 
     # Skip to the next reading if a valid measurement couldn't be taken.
     # This might happen if the CPU is under a lot of load and the sensor
     # can't be reliably read (timing is critical to read the sensor).
-    if humidity is None or temp is None:
-        print "waiting for 2 second"
-        time.sleep(FREQUENCY_SECONDS)
-        continue
-    else:
-        data = [
-            {
-                'time': time.time(),
-                'name': 'temperature',
-                'value': temp
-            },
-            {
-                'time': time.time(),
-                'name': 'humidity',
-                'value': humidity
-            }
-        ]
-        json_data = "%s\n" % (json.dumps(data))
+    data = [
+        {
+            'time': time1,
+            'name': 'temp1',
+            'value': temp1
+        },
+        {
+            'time': time1,
+            'name': 'humidity1',
+            'value': humidity1
+        },
+        {
+            'time': time2,
+            'name': 'temp2',
+            'value': temp2
+        },
+        {
+            'time': time2,
+            'name': 'humidity2',
+            'value': humidity2
+        },
+        {
+            'time': time3,
+            'name': 'temp3',
+            'value': temp3
+        },
+        {
+            'time': time3,
+            'name': 'humidity3',
+            'value': humidity3
+        }
+    ]
+    json_data = "%s\n" % (json.dumps(data))
 
     try:
         print "%s" % (json_data)
@@ -62,5 +99,3 @@ while True:
     except:
         print "problem writing to file"
         continue
-
-    time.sleep(FREQUENCY_SECONDS)
