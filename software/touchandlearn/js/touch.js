6a0388379cd6bd88875a4a3889ebcd6a930841e7
Bobby Cox <bobby.cox@berkeley.edu>
2016-05-19 15:47:57 -0700

    created first stab at touchscreen display code

---
+++ software/touchandlearn/js/touch.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#rect1').click(function(){
+    bootbox.dialog({
+      title: "Video 1",
+      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+
+  $('#ellipse1').click(function(){
+    bootbox.dialog({
+      title: "Video 1",
+      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+
+  $('#path1').click(function(){
+    bootbox.dialog({
+      title: "Video 1",
+      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+});
