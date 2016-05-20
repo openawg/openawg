c03ccb9fcca86f70d1dd5e32b71ea0784273ab80
Bobby Cox <bobby.cox@berkeley.edu>
2016-05-20 13:09:21 -0700

    updated with youtube videos and actual functionality

--- software/touchandlearn/js/touch.js
+++ software/touchandlearn/js/touch.js
@@ -1,24 +1,47 @@
 $(document).ready(function(){
-  $('.st21').click(function(){
+  $('.ro').click(function(){
     bootbox.dialog({
       animate: false,
       backdrop: false,
-      title: "Video 1",
-      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+      title: "How does Reverse Osmosis Work?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/aVdWqbpbv_Y?autoplay=1" frameborder="0" allowfullscreen></iframe>'
     });
   });
 
-  $('#ellipse1').click(function(){
+  $('.electrostatic').click(function(){
     bootbox.dialog({
-      title: "Video 1",
-      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc" frameborder="0" allowfullscreen></iframe>'
+      title: "What are electrostatic air filters?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/0GTAhLL_IV4?autoplay=1" frameborder="0" allowfullscreen></iframe>'
     });
   });
 
-  $('#path1').click(function(){
+  $('.evaporation').click(function(){
     bootbox.dialog({
-      title: "Video 1",
-      message: '<iframe width="696" height="391" src="https://www.youtube.com/embed/y2-uaPiyoxc" frameborder="0" allowfullscreen></iframe>'
+      title: "What is evaporation?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/r8M7mah_QaY?list=PLKbu9ieLIl_cd60FiPhiBrgRgnvnb44eI?autoplay=1" frameborder="0" allowfullscreen></iframe>'
     });
   });
+
+
+  $('.ozone-generator').click(function(){
+    bootbox.dialog({
+      title: "What are ozone generators?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/iWpdO55uFFA?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+
+  $('.condenser').click(function(){
+    bootbox.dialog({
+      title: "How do condensers and the refrigeration process work?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/h5wQoA15OnQ?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+
+  $('.compressor').click(function(){
+    bootbox.dialog({
+      title: "How do compressors work?",
+      message: '<iframe width="100%" height="600" src="https://www.youtube.com/embed/S08sj8pfJJs?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+    });
+  });
+
 });
